67ec8f36b9457f84d6c7af05c2e1db4ccd99d27f
Alecu Marian <a.marian.alexandru@gmail.com>
2017-10-15 14:57:08 +0300

    Generate Authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,20 +1,27 @@
 require('mongoose-type-email');
-
+// user-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const {Schema} = mongooseClient;
-  const user = new Schema({
+  const user = new mongooseClient.Schema({
+
     email: {
       type: mongooseClient.SchemaTypes.Email,
       unique: true,
       required: [true, 'Email is required']
     },
-    password: {
-      type: String,
-      required: [true, 'Password is required']
-    },
-    createdAt: {type: Date, default: Date.now},
-    updatedAt: {type: Date, default: Date.now}
+    password: { type: String, required: [true, 'Password is required'] },
+
+    auth0Id: { type: String },
+
+    googleId: { type: String },
+
+    facebookId: { type: String },
+
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
   });
 
   return mongooseClient.model('user', user);
